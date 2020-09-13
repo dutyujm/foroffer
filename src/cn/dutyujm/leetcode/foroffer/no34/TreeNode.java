c7d48e3d0561501d94b32cdb7a2e2e2e07341de0
鸣玉 <junming.yjm@alibaba-inc.com>
2020-09-13 17:27:22 +0800

    u

--- src/cn/dutyujm/leetcode/foroffer/no34/TreeNode.java
+++ src/cn/dutyujm/leetcode/foroffer/no34/TreeNode.java
@@ -8,18 +8,4 @@ public class TreeNode {
     TreeNode right;
     TreeNode(int x) { val = x; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TreeNode treeNode = (TreeNode) o;
-        return val == treeNode.val &&
-                left.equals(treeNode.left) &&
-                right.equals(treeNode.right);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(val);
-    }
 }
